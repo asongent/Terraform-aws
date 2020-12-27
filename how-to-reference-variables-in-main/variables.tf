6e60c1fafc3916e0399d36d1b6573931a4501412
asongent <59109755+asongent@users.noreply.github.com>
2020-12-27 13:24:26 -0500

    Update variables.tf

--- how-to-reference-variables-in-main/variables.tf
+++ how-to-reference-variables-in-main/variables.tf
@@ -1,19 +1,30 @@
-variable "sudo_password" {
-type = string
-default = "password"
-description = "This password is going to be unique for any resources that will be created in my accout"
-}
+#General Information: Variables consist of username and password are going to be 
+#used on resources that are username and password dependent.
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 variable "user_name" {
    type = string
    default = "admin"
    description = "This username is going to be unique accross all my aws resources"
 }
+
+variable "sudo_password" {
+type = string
+default = "password"
+description = "This password is going to be unique for any resources that will be created in my accout"
+}
  
+
+#This is the available zone(AZ) where all my aws resources are going to be deployed.
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 variable "AZ_name" {
   type    = string
   default = "us-east-1a"
 }
+
+#This variables are to be referenced on the respected feature  of EC2 Instance on main.tf
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 variable "image_id" {
   type = string
@@ -34,21 +45,15 @@ variable "name_tags" {
 }
 
 
+#VPC where all the resources will be hosted. In this vpc, there will be
+#a surity group that will defind ingress and engress rules.
+#The VPC and security will as well have name.
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 variable "vpc_id" {
     type = string
     default = "192.168.1.0/24"
     description = "This is for vpc testing. You can only try when you know Terraform"
-}
-
-variable "name" {
-    type = string
-    default = "honor-tracy"
- description = "This name is for vpc name"
-} 
-variable "s3name" {
-   type = string
-   default = "honor-tracy-bucket"
-description = "This bucket name"
 }
 
 variable "security_group" {
@@ -56,6 +61,24 @@ variable "security_group" {
    default = "honor-tracy-security-group"
 }
 
+variable "name" {
+    type = string
+    default = "honor-tracy"
+ description = "This name is for vpc name" 
+   }
+
+
+#These variables will be referenced on semple storage service (s3) on maint.tf.
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+variable "s3name" {
+   type = string
+   default = "honor-tracy-bucket"
+description = "This bucket name"
+}
+
+#These variables will be referenced on DB n main.tf.
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 variable "storage_allo" {
   type = string 
   default = "20"
@@ -82,6 +105,10 @@ variable "class" {
   description = "mysql db instance class"
 }
 
+#These can be incluted in the main.tf. It will print out output or features of any 
+#resources that you want to see. On the output below, 
+#I want to see the public and private ip addresses of my ec2 instance. 
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 output "public_ip" {
     value = aws_instance.tracy.*.public_ip
 }
